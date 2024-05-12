a305169be86d9a6b039e8110beee85758b524280
Mohit Sharma <mohleo.sharma@gmail.com>
2024-05-12 16:46:15 +0530

    Fetching and Displaying Users from Reqres API

---
+++ 12_may_2/index.js
@@ -0,0 +1,20 @@
+document.querySelector("button").addEventListener("click", renderCard)
+var container = document.querySelector(".container")
+async function renderCard() {
+  var response = await fetch(`https://reqres.in/api/users`)
+  var json = await response.json()
+  container.innerHTML = ""
+  var html = ""
+  json.data.forEach(each => {
+    html += `
+    <div class="card" style="">
+      <img class="card-img-top" src="${each.avatar}" alt="Avatar Image">
+      <div class="card-body">
+        <h2 class="card-title">${each.first_name} ${each.last_name}</h2>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum autem omnis odit mollitia deserunt temporibus, harum exercitationem quam.</p>
+        <a href="#" class="card-link">${each.email}</a>
+      </div>
+    </div>`
+  })
+  container.innerHTML = html
+}
