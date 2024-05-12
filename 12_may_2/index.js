fc94ab2116dc576a856ad6b2a125acbbd537122f
Mohit Sharma <mohleo.sharma@gmail.com>
2024-05-12 16:55:40 +0530

    Fetching and Displaying Users from Reqres API

--- 12_may_2/index.js
+++ 12_may_2/index.js
@@ -10,7 +10,7 @@ async function renderCard() {
     <div class="card" style="">
       <img class="card-img-top" src="${each.avatar}" alt="Avatar Image">
       <div class="card-body">
-        <h2 class="card-title">${each.first_name} ${each.last_name}</h2>
+        <h4 class="card-title">${each.first_name} ${each.last_name}</h4>
         <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum autem omnis odit mollitia deserunt temporibus, harum exercitationem quam.</p>
         <a href="#" class="card-link">${each.email}</a>
       </div>
